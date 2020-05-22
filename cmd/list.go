d3ea23f5bb290499fbf8328ef3b9a8af52accb5f
Anderson Braz <braz_anderson@msn.com>
2020-05-21 21:32:52 -0300

    Up Files

--- cmd/list.go
+++ cmd/list.go
@@ -25,12 +25,7 @@ import (
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `List all itens of TODO.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list called")
 	},
