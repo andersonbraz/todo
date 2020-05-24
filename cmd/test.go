166a62791161a65a4aa2ba8b76ec90927f5d19e0
Anderson Braz <braz_anderson@msn.com>
2020-05-24 14:46:13 -0300

    Up Files

--- cmd/test.go
+++ cmd/test.go
@@ -10,13 +10,8 @@ import (
 // testCmd represents the test command
 var testCmd = &cobra.Command{
 	Use:   "test",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Get Variables Enviroment",
+	Long:  `Execute Command: ./todo test`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("My HOME:", os.Getenv("HOME"))
 		fmt.Println("My GOPATH:", os.Getenv("GOPATH"))
